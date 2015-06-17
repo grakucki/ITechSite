322de525903b61e6d9655ff152581bd3debb25a3
grakucki <grakucki@insofter.pl>
2015-06-17 11:01:22 +0200

    dodanie PdfViewerControl

---
+++ InstrukcjeProdukcyjne/InstrukcjeProdukcyjne/PdfViewerControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InstrukcjeProdukcyjne
+{
+    public partial class PdfViewerControl : UserControl
+    {
+        public PdfViewerControl()
+        {
+            InitializeComponent();
+        }
+
+        public string FileName { get; set; }
+        private void PdfViewerControl_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
