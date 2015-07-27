ea48f899b00e1f3ecad9b476a49e2ddfc428c310
grakucki <grakucki@insofter.pl>
2015-07-27 12:15:22 +0200

    Custom panels

---
+++ InstrukcjeProdukcyjne/InstrukcjeProdukcyjne/DokumentShowDlg.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InstrukcjeProdukcyjne
+{
+    public partial class DokumentShowDlg : Form
+    {
+        public DokumentShowDlg(MyFileInfo file, SitechUser user)
+        {
+            File= file;
+            User = user;
+            InitializeComponent();
+        }
+
+        public MyFileInfo File { get; set; }
+        public SitechUser User { get; set; }
+
+
+        private void GoFullscreen(bool fullscreen)
+        {
+            if (fullscreen)
+            {
+                this.WindowState = FormWindowState.Normal;
+                this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+                this.Bounds = Screen.PrimaryScreen.Bounds;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Maximized;
+                this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+            }
+
+        }
+
+        private void DokumentShowDlg_Load(object sender, EventArgs e)
+        {
+             GoFullscreen(true);
+            if (User != null)
+            {
+                labelUserName.Text = User.UserName;
+                labelUserNo.Text = User.NrKarty;
+            }
+            if (File != null)
+            {
+                KomunikatLabel.Text = File.Dok.Description;
+                mediaViewerControl1.ShowDokument(File.FullFileName);
+            }
+            else
+            {
+                MessageBox.Show("Nie określono dokumentu");
+                this.Close();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
