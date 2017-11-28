34165ca5e0691ceffd2b8c925a4ee5d96a3956cc
Kowalczyk-insofter <kowalczyk@insofter.pl>
2017-11-28 09:15:21 +0100

    dodanie konrtolera faktorys i projects

---
+++ ITechSite/Areas/Settings/Controllers/ProjectsController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITechSite.Areas.Settings.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        // GET: Settings/Projects
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Settings/Projects/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Settings/Projects/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Settings/Projects/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Settings/Projects/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Settings/Projects/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Settings/Projects/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Settings/Projects/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ITechSite/Areas/Settings/SettingsAreaRegistration.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace ITechSite.Areas.Settings
+{
+    public class SettingsAreaRegistration : AreaRegistration 
+    {
+        public static string Name
+        {
+            get
+            {
+                return "Settings";
+            }
+        }
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Settings";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Settings_default",
+                "Settings/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+              
+           );
+       }
+    }
+}
