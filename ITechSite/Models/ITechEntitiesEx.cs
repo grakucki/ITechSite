8f4fb5eb528d29b086bb52d0275e7ca4c26bb83c
Kowalczyk-insofter <kowalczyk@insofter.pl>
2018-01-15 18:37:02 +0100

    clean

--- ITechSite/Models/ITechEntitiesEx.cs
+++ ITechSite/Models/ITechEntitiesEx.cs
@@ -27,14 +27,7 @@ namespace ITechSite.Models
             var c = ConfigurationManager.ConnectionStrings[name];
             var comp = Environment.MachineName;
             string ret = string.Empty;
-            if (comp.ToLower() == "akacjowa5")
-            {
-                if (c != null)
-                {
-                    ret = ReplaceCaseInsensitive(c.ConnectionString,@"data source=COOLER7\SQLEXPRESS", @"data source=Akacjowa5\");
-                }
-            }
-            else
+            
                 ret =name;
             return ret;
         }
@@ -44,8 +37,7 @@ namespace ITechSite.Models
 
             int i = 1;
             var comp = Environment.MachineName;
-            if (comp == "AKACJOWA5")
-                i = 2;
+           
             String n=string.Empty;
             switch (i)
             {
@@ -53,14 +45,7 @@ namespace ITechSite.Models
                     break;
                 case 1: n = "name=ITechEntities2";
                     break;
-                case 2:
-                    {
-                        var c = ConfigurationManager.ConnectionStrings["ITechEntities2"];
-                        n = c.ConnectionString.Replace(@"data source=COOLER7\SQLEXPRESS",@"data source=Akacjowa5\" );
-                        //n = "name=ITechEntities2";
-                        //n = @"metadata=res://*/Models.ITechModel.csdl|res://*/Models.ITechModel.ssdl|res://*/Models.ITechModel.msl;provider=System.Data.SqlClient;provider connection string='data source=Akacjowa5\;initial catalog=ITech;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework'";
-                    }
-                    break;
+                
                 default:
                     n = "name=ITechEntities";
                     break;
